0b8e98e44a442d6d4e9ba36562467a9484728287
Александр Шмидт <aleksandrsmidt@MacBook-Pro-Aleksandr.local>
2019-08-25 00:44:34 +0300

    working version setted launched

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,27 @@
+<template>
+	 <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="word_card"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloVue",
+  props: {
+    msg: String
+  }
+};
+</script>
+<style>
+  .word_card {
+    height: 300px;
+    width: 400px;
+    background-color: lightgreen;
+    border: 3px solid green;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: stretch; 
+  }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
+
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Here is the page, where I will learn vue!</h1>
+    <HelloVue msg="Hello, Your Magesty, Vue"/>
   </div>
 </template>
+
+<script>
+import HelloVue from "@/components/HelloVue.vue";
+
+export default {
+  name: "about",
+  components: {
+  	HelloVue
+  }
+};
+</script>
+<style>
+	.about {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: center; 
+		background-color: rgba(220, 220, 200, 0.5);
+	}
+</style>
